a136d3b25437be3ea9f0b515359fc37923472982
Cesar <50472267+EmilCes@users.noreply.github.com>
2024-02-26 09:42:12 -0600

    First Commit

--- src/main/java/com/softdev/fmsb/config/SecurityConfig.java
+++ src/main/java/com/softdev/fmsb/config/SecurityConfig.java
@@ -11,8 +11,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
