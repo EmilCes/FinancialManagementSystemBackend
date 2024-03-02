a14ca360ad6c2026094273e55fad7dc44cb43629
Cesar <50472267+EmilCes@users.noreply.github.com>
2024-03-01 19:11:37 -0600

    Refresh Token Implementation

--- src/main/java/com/softdev/fmsb/token/TokenRepository.java
+++ src/main/java/com/softdev/fmsb/token/TokenRepository.java
@@ -1,6 +1,5 @@
 package com.softdev.fmsb.token;
 
-import com.softdev.fmsb.token.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
